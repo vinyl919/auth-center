3d195a28eb9938e468bb8272348ef8dc002f4b2c
xxx <xxx@xxx-ThinkCentre-M58>
2014-11-19 15:34:38 +0100

    logout btn

---
+++ src/Acme/CertBundle/Entity/ClientCertificate.php
@@ -0,0 +1,15 @@
+<?php
+namespace Acme\CertBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="clientCertificates")
+ * @UniqueEntity(fields="certName", message="Email already taken")
+ */
+class ClientCertificate {
+	
+	
+}
